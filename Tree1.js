c17e89fd3bf4a2a0916b5d05c6ed77275342b598
krashdar <75021815+krashdar@users.noreply.github.com>
2021-05-18 17:10:17 +0700

    step3

--- Tree1.js
+++ Tree1.js
@@ -14,28 +14,33 @@ class Tree extends Node{
 }
 
 let TreeNode = document.querySelector('.tree_items');
+let TreeAttrNode = document.querySelector('.tree_item_attr');
 const btnJSON = document.querySelector('.j-btn-JSON');
 const btnTreeTODOM = document.querySelector('.j-btn-TreeToDOM');
+const btnAddElement = document.querySelector(".j-btn-AddElement");
+const btnRename = document.querySelector(".j-btn-Rename");
+const btnDelete = document.querySelector(".j-btn-Delete");
+const btnAddAttr = document.querySelector(".j-btn-AddAttr");
+const btnDeleteAttr = document.querySelector(".j-btn-DeleteAttr");
+const btnDeleteOneAttr = document.querySelector(".j-btn-DeleteOneAttr");
 
-//TODO придумать где хранить атрибуты на странице и их вывод
 function TreeToDOM (tree,treeNode) {
-    let i = tree.children.length;
-    while (i>0){
+    let i = 0;
+    while (i<tree.children.length){
         let li = document.createElement("li");
-        li.textContent = tree.children[i-1].name;
-        if (tree.children[i-1].children.length !==0){
+        li.textContent = tree.children[i].name;
+        if (tree.children[i].children.length !==0){
             let ul = document.createElement("ul");
             li.appendChild(ul);
-            TreeToDOM (tree.children[i-1],ul)
+            TreeToDOM (tree.children[i],ul)
         }
         treeNode.appendChild(li);
-    i--;
+    i++;
     }
 }
-//TODO придумать где хранить атрибуты на странице и в зависимости от этого собирать их в дерево для хранения
+
 function DOMToTree (treeNode){
     let tree = new Tree("Моё дерево");
-
     recurse(treeNode,tree)
     function recurse(nodeNode,nodeObj){
         for (let elem of nodeNode.children){
@@ -56,6 +61,7 @@ function DOMToTree (treeNode){
     }
     return tree;
 }
+
 function FindElem (tree,name){
     if (tree.name === name){
         return  tree;
@@ -77,16 +83,23 @@ function FindElem (tree,name){
     }
 }
 
-const btnAddAttr = document.querySelector(".j-btn-AddAtr");
-function changeButton(btn,activeElement){
+function changeButton(activeElement){
     btnAddAttr.textContent = `создать атрибут для ${activeElement.name}`;
+    btnDeleteAttr.textContent = `удалить атрибуты у ${activeElement.name}`;
+}
+function changeText(activeElement){
+    let text = "";
+    for (let item of activeElement.attributes){
+        text += `Атрибут - ${item[0]}, Значение - ${item[1]}\n`;
+    }
+    TreeAttrNode.textContent = text;
 }
 
 
 let tree = DOMToTree(TreeNode)
 let activeElementNode = TreeNode;
 let activeElement = tree;
-changeButton(btnAddAttr,activeElement);
+changeButton(activeElement);
 const TreeItemsNode = document.querySelector('.tree_items_div');
 
 TreeItemsNode.onclick = function (e) {
@@ -99,15 +112,10 @@ TreeItemsNode.onclick = function (e) {
     activeElement = FindElem(tree,findName[0]);
     console.log(activeElement);
 
-    changeButton(btnAddAttr,activeElement);
+    changeText(activeElement);
+    changeButton(activeElement);
 
 
-    // activeElementNode.active = !activeElementNode.active;
-    // if (activeElementNode.active){
-    //     activeElementNode.classList.add('active');
-    // }else {
-    //     activeElementNode.classList.remove('active')
-    // }
 
     //TODO переделать выделение элемента и снятие выделения у всех остальных элементов
 
@@ -124,8 +132,42 @@ TreeItemsNode.onclick = function (e) {
     //     activeElementNode.classList.remove('active')
     // }
 }
+btnAddElement.addEventListener("click", () => {
+    let newElementName = prompt('Введите имя нового элемента');
+    let newElement = new Node(newElementName);
+    newElement.parent = activeElement.name;
+    activeElement.children.push(newElement);
+    changeText(activeElement);
+    changeButton(activeElement);
+    activeElement = newElement;
+    TreeNode.innerHTML = "";
+    TreeToDOM(tree,TreeNode);
+})
+btnRename.addEventListener("click", () => {
+    let elementName = prompt('Введите новое имя элемента');
+    if (elementName === null){return;}
+    activeElement.name = elementName;
+    changeText(activeElement);
+    changeButton(activeElement);
+    TreeNode.innerHTML = "";
+    TreeToDOM(tree,TreeNode);
+})
+btnDelete.addEventListener("click", () => {
+    if (activeElement.parent !== null){
+        let parent = FindElem(tree,activeElement.parent);
+        let index = parent.children.indexOf(activeElement);
+        if (index > -1){
+            parent.children.splice(index,1);
+        }
+        activeElement = parent;
+        changeText(activeElement);
+        changeButton(activeElement);
+        TreeNode.innerHTML = "";
+        TreeToDOM(tree,TreeNode);
+    }
 
-//TODO придумать где хранить атрибуты и возможно, выводить после записи
+})
+
 btnAddAttr.addEventListener("click", () => {
     let key = prompt('Введите название атрибута');
     let value = prompt('Введите значение атрибута');
@@ -133,11 +175,24 @@ btnAddAttr.addEventListener("click", () => {
         activeElement.attributes.set(key,value);
     }
     console.log(activeElement);
+    changeText(activeElement);
+    changeButton(activeElement);
 });
 
-btnJSON.addEventListener("click", ()=>{
-    console.log(JSON.stringify(DOMToTree(TreeNode)));
+btnDeleteAttr.addEventListener("click", () => {
+    activeElement.attributes.clear();
+    changeText(activeElement);
 })
+btnDeleteOneAttr.addEventListener("click", () => {
+    let find = prompt('Введите искомый ключ атрибута для удаления');
+    activeElement.attributes.delete(`${find}`);
+    changeText(activeElement);
+})
+
+btnJSON.addEventListener("click", ()=>{
+    console.log(JSON.stringify(tree));
+})
+
 btnTreeTODOM.addEventListener("click",() => {
     let data = DOMToTree(TreeNode);
     TreeNode.innerHTML = "";
